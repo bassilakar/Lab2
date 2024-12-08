94a24f6921474f932dafd2ccb89826a73c28317e
Bassil Akar <bakar@myseneca.ca>
2024-12-08 18:04:17 -0500

    lab Done

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Bassil Akar
+# Author ID: bakar
+#Date Created: 2024/12/8
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
